5272fb3425ea632da06d33c355c0175f3b41fa61
Mads Jensen <madsaj10@outlook.com>
2025-01-23 12:38:25 +0100

    Add hyprland animation conf

--- home/shared/hyprland.nix
+++ home/shared/hyprland.nix
@@ -67,5 +67,17 @@
             }" # Move window to workspace
           ]) 9));
     };
+
+    animations = {
+      enabled = "yes";
+      bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+      animation = [
+        "windows, 1, 5, myBezier"
+        "windowsOut, 1, 7, default, popin 80%"
+        "border, 1, 10, default"
+        "fade, 1, 7, default"
+        "workspaces, 1, 6, default"
+      ];
+    };
   };
 }
