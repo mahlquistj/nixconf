3feedede10962fa1b7528f705e547525c7753a00
Mads Jensen <madsaj10@outlook.com>
2025-01-23 10:30:42 +0100

    quote hyprland colors + add some keybinds

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 Inspiration for layout: https://github.com/namishh/crystal/tree/main
-
-Hardware configuration should be put into the `config` directory
--- home/shared/hyprland.nix
+++ home/shared/hyprland.nix
@@ -8,6 +8,8 @@
 
     settings = {
       "$mod" = "SUPER";
+      "$modshift" = "$mod SHIFT";
+
       "$terminal" = "alacritty";
       "$fileManager" = "nemo";
       "$menu" = "wofi --show drun";
@@ -18,22 +20,39 @@
 
         border_size = 2;
 
-        col = {
-          active_border = "rgba(${colors.color1}ee)";
-          inactive_border = "rgba(${colors.color0}aa)";
-        };
+        "col.active_border" = "rgba(${colors.color1}ee)";
+        "col.inactive_border" = "rgba(${colors.color0}aa)";
       };
 
-      bind = [ "$mod, F, exec, firefox" ", Print, exec, grimblast copy area" ]
-        ++ (
-          # workspaces
-          # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-          builtins.concatLists (builtins.genList (i:
-            let ws = i + 1;
-            in [
-              "$mod, code:1${toString i}, workspace, ${toString ws}"
-              "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-            ]) 9));
+      bind = [
+        # Spawners
+        "$mod, ENTER, exec, $terminal"
+        "$mod, W, exec, firefox"
+
+        # Moving windows
+        "$mod, left, movefocus, l"
+        "$mod, right, movefocu, r"
+        "$mod, up, movefocus, u"
+        "$mod, down, movefocus, d"
+
+        # Window control
+        "$modshift, V, togglefloating"
+        "$modshift, J, togglesplit"
+        "$modshift, F, fullscreen"
+        "$modshift, Q, killactive"
+      ] ++ (
+        # workspaces
+        # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+        builtins.concatLists (builtins.genList (i:
+          let ws = i + 1;
+          in [
+            "$mod, code:1${toString i}, workspace, ${
+              toString ws
+            }" # Switch workspace
+            "$modshift, code:1${toString i}, movetoworkspace, ${
+              toString ws
+            }" # Move window to workspace
+          ]) 9));
     };
   };
 }
