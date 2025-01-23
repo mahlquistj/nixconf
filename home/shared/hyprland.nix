e1547f4d0a9ea218c2be6d1b941af53da3ee14be
Mads Jensen <madsaj10@outlook.com>
2025-01-23 13:11:02 +0100

    added a lot of styling

--- home/shared/hyprland.nix
+++ home/shared/hyprland.nix
@@ -7,34 +7,64 @@
     plugins = [ ];
 
     settings = {
+      # Important keys
       "$mod" = "SUPER";
-      "$modshift" = "SUPER_SHIFT";
+      "$modshift" = "SUPER SHIFT";
 
+      # Apps
       "$terminal" = "alacritty";
-      "$fileManager" = "nemo";
+      "$fileman" = "nemo";
       "$menu" = "wofi --show drun";
+      "$browser" = "firefox";
 
+      # Startup
       exec-once = "$terminal";
 
+      # Setttings and styling
       general = {
         gaps_in = 5;
-        gaps_out = 20;
+        gaps_out = 10;
 
-        border_size = 2;
+        border_size = 1;
 
         "col.active_border" = "rgba(${colors.color1}ee)";
         "col.inactive_border" = "rgba(${colors.color0}aa)";
+
+        resize_on_border = true;
+      };
+      decoration = {
+        rounding = 2;
+
+        inactive_opacity = 0.9;
+        dim_inactive = true;
+      };
+      input = { kb_layout = "dk"; };
+      gestures = {
+        workspace_swipe = true;
+
+      };
+      animations = {
+        enabled = "yes";
+        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+        animation = [
+          "windows, 1, 5, myBezier"
+          "windowsOut, 1, 7, default, popin 80%"
+          "border, 1, 10, default"
+          "fade, 1, 7, default"
+          "workspaces, 1, 6, default"
+        ];
       };
 
-      input = { kb_layout = "dk"; };
-
+      # Bindings
       bind = [
         # Important controls
         "$modshift, ESCAPE, exit"
 
         # Spawners
         "$mod, Return, exec, $terminal"
-        "$mod, W, exec, firefox"
+        "$mod, W, exec, $browser"
+        "$mod, F, exec, $fileman"
+        "$mod, R, exec, $menu"
 
         # Moving windows
         ## Arrow keys
@@ -66,18 +96,6 @@
               toString ws
             }" # Move window to workspace
           ]) 9));
-    };
-
-    animations = {
-      enabled = "yes";
-      bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
-      animation = [
-        "windows, 1, 5, myBezier"
-        "windowsOut, 1, 7, default, popin 80%"
-        "border, 1, 10, default"
-        "fade, 1, 7, default"
-        "workspaces, 1, 6, default"
-      ];
     };
   };
 }
