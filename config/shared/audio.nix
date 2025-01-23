d4c5b637f4a3cf4fd4809ed0d3e5d57a0edfda9d
Mads Jensen <madsaj10@outlook.com>
2025-01-23 01:43:05 +0100

    remove audio settings for now - TODO

--- config/shared/audio.nix
+++ config/shared/audio.nix
@@ -1,9 +1,5 @@
 { config, pkgs, ... }:
 
 {
-  hardware.pulseaudio = {
-    enable = true;
-    extraConfig =
-      "load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1";
-  };
+  #TODO
 }
