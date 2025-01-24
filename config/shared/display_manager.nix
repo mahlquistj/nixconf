2fdf72d4b6417a713fb4bb504e82d0850a8af685
Mads Jensen <madsaj10@outlook.com>
2025-01-24 15:03:07 +0100

    remove sddm

--- config/shared/display_manager.nix
+++ config/shared/display_manager.nix
@@ -1,26 +1,16 @@
-{ pkgs, wallpapers, colors, ... }:
-let
-  sddm-theme = (pkgs.where-is-my-sddm-theme.override {
-    themeConfig.General = {
-      background = "${wallpapers}/login-background.png";
-      backgroundMode = "fill";
+{ pkgs, wallpapers, colors, ... }: {
+  environment.systemPackages = [
+    (pkgs.where-is-my-sddm-theme.override {
+      themeConfig.General = {
+        background = "${wallpapers}/login-background.png";
+        backgroundMode = "fill";
 
-      passwordMask = true;
-      passwordInputBackground = "${colors.background}";
-      passwordInputRadius = 8;
-      passwordInputVisibleCursor = true;
+        passwordMask = true;
+        passwordInputBackground = "${colors.background}";
+        passwordInputRadius = 8;
+        passwordInputVisibleCursor = true;
+      };
+    })
+  ];
 
-    };
-  });
-
-in {
-  environment.systemPackages = [ sddm-theme ];
-
-  services.displayManager.sddm = {
-    enable = true;
-    wayland.enable = true;
-    theme = "where-is-my-sddm";
-    settings = { Theme = { Current = "where-is-my-sddm"; }; };
-    extraPackages = [ sddm-theme ];
-  };
 }
