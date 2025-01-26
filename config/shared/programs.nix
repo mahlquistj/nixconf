bcfe1e0630535a7033d12c45bb4a8c38f5973638
Mads Jensen <madsaj10@outlook.com>
2025-01-26 23:07:57 +0100

    remove redshift

--- config/shared/programs.nix
+++ config/shared/programs.nix
@@ -33,18 +33,5 @@
   services = {
     # OpenSSH
     openssh.enable = true;
-
-    redshift = {
-      enable = true;
-      brightness = {
-        day = "1";
-        night = "1";
-      };
-      temperature = {
-        day = 5500;
-        night = 3700;
-      };
-    };
   };
-
 }
