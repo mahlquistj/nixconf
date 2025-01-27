faa8b54a71b4a8f745f270e62aae66451fcf75a0
Mads Jensen <madsaj10@outlook.com>
2025-01-27 13:21:49 +0100

    debug plugged

--- home/shared/waybar.nix
+++ home/shared/waybar.nix
@@ -78,17 +78,16 @@ in {
         interval = 20;
 
         format = "{icon} {capacity}%";
-
         format-full = "{icon} FULL {capacity}%";
         format-good = "{icon} GOOD {capacity}%";
         format-warning = "{icon} WARN {capacity}% {time}";
         format-critical = "{icon} CRIT {capacity}% {time}";
         
-        format-plugged = "󱐋{icon} FULL {capacity}% {time}";
-        format-plugged-full = "󱐋{icon} FULL {capacity}% {time}";
-        format-plugged-good = "󱐋{icon} GOOD {capacity}% {time}";
-        format-plugged-warning = "󱐋{icon} WARN {capacity}% {time}";
-        format-plugged-critical = "󱐋{icon} CRIT {capacity}% {time}";
+        format-charging = "󱐋{icon} CFULL {capacity}% {time}";
+        format-charging-full = "󱐋{icon} CFULL {capacity}% {time}";
+        format-charging-good = "󱐋{icon} CGOOD {capacity}% {time}";
+        format-charging-warning = "󱐋{icon} CWARN {capacity}% {time}";
+        format-charging-critical = "󱐋{icon} CCRIT {capacity}% {time}";
 
         states = {
           full = 99;
