72509b3c8e4f213f1729904f4fd3d6fa488aa9e4
Mads Jensen <madsaj10@outlook.com>
2025-01-27 19:44:57 +0100

    format date

--- home/shared/waybar.nix
+++ home/shared/waybar.nix
@@ -70,26 +70,31 @@ in {
       #smallspacer = { "format" = " "; };
 
       "hyprland/workspaces" = { 
-        format = "{icon}";
-        format-icons = {
-          "1" = "󱄅";
-          "2" = "󰖟";
-          "3" = "󱘗";
-          "4" = "";
-          "5" = "󰓇";
-          "6" = "6";
-          "7" = "7";
-          "8" = "8";
-          "9" = "9";
-        };
+        format = "{name}";
+        #format-icons = {
+        #  "1" = "󱄅";
+        #  "2" = "󰖟";
+        #  "3" = "󱘗";
+        #  "4" = "";
+        #  "5" = "󰓇";
+        #  "6" = "6";
+        #  "7" = "7";
+        #  "8" = "8";
+        #  "9" = "9";
+        #};
         persistent-workspaces = {
           "*" = 5;
         };
       };
+
       "hyprland/window" = {
         format = "{title}";
         icon = true;
         icon-size = 15;
+      };
+
+      clock = {
+        format = "{%H:%M %a %b %d %Y}";
       };
 
       battery = {
