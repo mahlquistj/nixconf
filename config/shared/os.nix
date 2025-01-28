7a05a3c47628d26d021971b0f9d26991e7ee4eca
Mads Jensen <madsaj10@outlook.com>
2025-01-28 22:50:37 +0100

    Change locales to en_DK

--- config/shared/os.nix
+++ config/shared/os.nix
@@ -25,15 +25,15 @@
   i18n = {
     defaultLocale = "en_DK.UTF-8";
     extraLocaleSettings = {
-      LC_ADDRESS = "da_DK.UTF-8";
-      LC_IDENTIFICATION = "da_DK.UTF-8";
-      LC_MEASUREMENT = "da_DK.UTF-8";
-      LC_MONETARY = "da_DK.UTF-8";
-      LC_NAME = "da_DK.UTF-8";
-      LC_NUMERIC = "da_DK.UTF-8";
-      LC_PAPER = "da_DK.UTF-8";
-      LC_TELEPHONE = "da_DK.UTF-8";
-      LC_TIME = "da_DK.UTF-8";
+      LC_ADDRESS = "en_DK.UTF-8";
+      LC_IDENTIFICATION = "en_DK.UTF-8";
+      LC_MEASUREMENT = "en_DK.UTF-8";
+      LC_MONETARY = "en_DK.UTF-8";
+      LC_NAME = "en_DK.UTF-8";
+      LC_NUMERIC = "en_DK.UTF-8";
+      LC_PAPER = "en_DK.UTF-8";
+      LC_TELEPHONE = "en_DK.UTF-8";
+      LC_TIME = "en_DK.UTF-8";
     };
   };
 
