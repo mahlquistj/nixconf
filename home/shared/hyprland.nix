752a3e3f95d52658021e5a85f7bb1084e6f3880b
Mads Jensen <madsaj10@outlook.com>
2025-01-29 13:25:12 +0100

    more debug

--- home/shared/hyprland.nix
+++ home/shared/hyprland.nix
@@ -148,7 +148,7 @@
         "$mod, W, exec, $browser"
         "$mod, F, exec, $fileman"
         "$mod, R, exec, $menu"
-        "$mod, D, exec, echo $PATH" # DEBUG
+        "$mod, D, exec, echo $PATH && whoami" # DEBUG
 
         # Moving windows
         ## Arrow keys
