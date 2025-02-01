1f556c08e4574c1d4d87e491c2d2dbcc571a9719
Mads Jensen <madsaj10@outlook.com>
2025-02-01 04:05:38 +0100

    add more aliases to fish

--- home/shared/fish.nix
+++ home/shared/fish.nix
@@ -17,12 +17,19 @@
       # ls -> eza
       "ls" = "eza -F --icons";
       "la" = "eza -laF --icons";
-      "lg" = "eza -laF --icons --git --git-ignore";
-      "lt" = "eza -laF --tree --icons";
-      "lt1" = "eza -laF --tree --icons --level 1";
-      "lt2" = "eza -laF --tree --icons --level 2";
-      "lt3" = "eza -laF --tree --icons --level 3";
-      "lt4" = "eza -laF --tree --icons --level 4";
+      "lt" = "la --tree";
+      "lt1" = "lt --level 1";
+      "lt2" = "lt --level 2";
+      "lt3" = "lt --level 3";
+      "lt4" = "lt --level 4";
+      "lt5" = "lt --level 5";
+      "lg" = "la --git --git-ignore";
+      "lgt" = "lt --git --git-ignore";
+      "lgt1" = "lt1 --git --git-ignore";
+      "lgt2" = "lt2 --git --git-ignore";
+      "lgt3" = "lt3 --git --git-ignore";
+      "lgt4" = "lt4 --git --git-ignore";
+      "lgt5" = "lt5 --git --git-ignore";
 
       # Various program aliases
       "top" = "btop";
