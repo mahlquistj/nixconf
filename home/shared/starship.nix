c098ee2bd104b9091dea1d7a2e08c5e2b641cd2b
Mads Jensen <madsaj10@outlook.com>
2025-02-02 05:08:41 +0100

    change color again

--- home/shared/starship.nix
+++ home/shared/starship.nix
@@ -11,7 +11,7 @@ in {
       fill.symbol = " ";
 
       format = multiline ''
-        [╭](fg:overlay0)
+        [╭](fg:surface0)
         $username
         $directory
 
@@ -27,7 +27,7 @@ in {
 
         $line_break
 
-        [╰](fg:overlay0)
+        [╰](fg:surface0)
         $character
       '';
 
@@ -35,13 +35,13 @@ in {
         format = multiline ''
           [](fg:peach)
           [](bg:peach fg:crust)
-          [](fg:peach bg:overlay0)
+          [](fg:peach bg:surface0)
           [ $user]($style)
-          [](fg:overlay0)
+          [](fg:surface0)
         '';
         show_always = true;
-        style_user = "bg:overlay0";
-        style_root = "fg:red  bg:overlay0";
+        style_user = "bg:surface0";
+        style_root = "fg:red  bg:surface0";
       };
 
       directory = {
@@ -49,38 +49,38 @@ in {
         read_only = "";
         home_symbol = "";
 
-        style = "bg:overlay0";
-        read_only_style = "bg:overlay0 fg:red";
+        style = "bg:surface0";
+        read_only_style = "bg:surface0 fg:red";
         before_repo_root_style = "none";
         repo_root_style = "none";
         format = multiline ''
-          [─](fg:overlay0)
+          [─](fg:surface0)
           [](fg:mauve)
           [](bg:mauve fg:crust)
-          [](fg:mauve bg:overlay0)
+          [](fg:mauve bg:surface0)
           [ $path]($style)
           [ $read_only]($read_only_style)
-          [](fg:overlay0)
+          [](fg:surface0)
         '';
         repo_root_format = multiline ''
-          [─](fg:overlay0)
+          [─](fg:surface0)
           [](fg:mauve)
           [](bg:mauve fg:crust)
-          [](fg:mauve bg:overlay0)
+          [](fg:mauve bg:surface0)
           [/$repo_root]($style)
           [ $path]($style)
           [ $read_only]($read_only_style)
-          [](fg:overlay0)
+          [](fg:surface0)
         '';
       };
 
       git_branch = {
         symbol = "";
         format = multiline ''
-          [─](fg:overlay0)
+          [─](fg:surface0)
           [](fg:green)
           [$symbol $branch(:$remote_branch)](fg:crust bg:green)
-          [](fg:green bg:overlay0)
+          [](fg:green bg:surface0)
         '';
       };
 
@@ -89,17 +89,17 @@ in {
         ahead = " 🏎";
         behind = " 😰";
         diverged = " 😵";
-        up_to_date = "[ ✓](bold fg:green bg:overlay0)";
+        up_to_date = "[ ✓](bold fg:green bg:surface0)";
         untracked = " 🤷";
         stashed = " 📦";
         modified = " 📝";
-        staged = "[ +$count](fg:green bg:overlay0)";
+        staged = "[ +$count](fg:green bg:surface0)";
         renamed = " 👅";
         deleted = " 🗑";
 
         format = multiline ''
-          [$conflicted$stashed$deleted$renamed$modified$typechanged$untracked$staged$ahead_behind](bg:overlay0)
-          [](fg:overlay0)
+          [$conflicted$stashed$deleted$renamed$modified$typechanged$untracked$staged$ahead_behind](bg:surface0)
+          [](fg:surface0)
         '';
       };
 
@@ -108,9 +108,9 @@ in {
         format = multiline ''
           [](fg:yellow)
           [󱐋](bold fg:crust bg:yellow)
-          [](fg:yellow bg:overlay0)
-          [ $duration](bg:overlay0)
-          [─](fg:overlay0)
+          [](fg:yellow bg:surface0)
+          [ $duration](bg:surface0)
+          [─](fg:surface0)
         '';
       };
 
@@ -119,9 +119,9 @@ in {
         format = multiline ''
           [](fg:sapphire)
           [](fg:crust bg:sapphire)
-          [](fg:sapphire bg:overlay0)
-          [ $time](bg:overlay0)
-          [ ](fg:overlay0)
+          [](fg:sapphire bg:surface0)
+          [ $time](bg:surface0)
+          [ ](fg:surface0)
         '';
       };
 
