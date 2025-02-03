77d54693f7f31bf05913d41805bdfc22f39ae131
Mads Jensen <madsaj10@outlook.com>
2025-02-03 15:48:33 +0100

    no modifier for volume controls

--- home/shared/hyprland.nix
+++ home/shared/hyprland.nix
@@ -140,8 +140,8 @@
       # Bindings
       binde = [
         # Volume up/down
-        "XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.4 @DEFAULT_AUDIO_SINK@ 5%+"
-        "XF86AudioLowerVolume, exec, wpctl set-volume -l 1.4 @DEFAULT_AUDIO_SINK@ 5%-"
+        ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.4 @DEFAULT_AUDIO_SINK@ 5%+"
+        ",XF86AudioLowerVolume, exec, wpctl set-volume -l 1.4 @DEFAULT_AUDIO_SINK@ 5%-"
       ];
       bind = [
         # Important controls
@@ -179,7 +179,7 @@
         "$modshift, PRINT, exec, hyprshot -m region"
 
         # Volume mute
-        "XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
       ] ++ (
         # workspaces
         # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
