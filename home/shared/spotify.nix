ca655f9e682ce925f9b65b270f7795bf978f234b
Mads Jensen <madsaj10@outlook.com>
2025-02-03 23:30:31 +0100

    try manual name for spicetify theme

--- home/shared/spotify.nix
+++ home/shared/spotify.nix
@@ -3,7 +3,7 @@ let spicePkgs = spicetify-nix.legacyPackages.${pkgs.stdenv.system};
 in {
   programs.spicetify = {
     enable = true;
-    theme = spicePkgs.themes.catppuccin;
+    theme = "mocha";
     colorScheme = config.catppuccin.flavor;
     enabledExtensions = with spicePkgs.extensions; [ fullAppDisplay shuffle ];
     enabledCustomApps = with spicePkgs.apps; [ newReleases ];
