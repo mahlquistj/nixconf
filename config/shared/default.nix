208322c0c9b9ed34ca6a1422d1aa3374474c012b
Mads Jensen <madsaj10@outlook.com>
2025-02-07 14:17:45 +0100

    remove legacy system configurations

--- config/shared/default.nix
+++ config/shared/default.nix
@@ -1,14 +1,37 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
     ./audio.nix
     ./display_manager.nix
     ./fonts.nix
-    ./os.nix
     ./programs.nix
     ./shell.nix
     /etc/nixos/hardware-configuration.nix
     /etc/nixos/configuration.nix
   ];
+
+  # Location
+  location.provider = "geoclue2";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.defaultUserShell = pkgs.fish;
+
+  security = {
+    polkit.enable = true;
+    pam.services.hyprlock = { };
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  environment.sessionVariables = {
+    # Hint electron apps to use wayland
+    NIXOS_OZONE_WL = "1";
+    ELECTRON_OZONE_PLATFORM_HINT = "auto";
+    GDK_SCALE = "1";
+  };
 }
