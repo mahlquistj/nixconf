24921366e8dcacea0db866cf1ad4d1bd16435e0a
Mads Jensen <madsaj10@outlook.com>
2025-02-12 12:19:17 +0100

    open rofi on SUPER + SPACE

--- home/shared/hyprland.nix
+++ home/shared/hyprland.nix
@@ -155,7 +155,7 @@
         "$mod, Return, exec, $terminal"
         "$mod, W, exec, $browser"
         "$mod, F, exec, $fileman"
-        "$mod, R, exec, $menu"
+        "$mod, SPACE, exec, $menu"
         "$mod, D, exec, echo $PATH && whoami" # DEBUG
 
         # Moving windows
