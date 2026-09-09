199f7fb451c1207b552a9921a90912d5a2d20228
Mads Ahlquist Jensen <madsaj10@outlook.com>
2026-09-09 22:00:52 +0200

    ollama don't keep alive

--- home/shared/ollama.nix
+++ home/shared/ollama.nix
@@ -5,10 +5,11 @@
     package = pkgs.ollama-rocm; # Ensures AMD ROCm acceleration is active
 
     environmentVariables = {
-      # 1. Sets the global default context length to 128k
-      OLLAMA_CONTEXT_LENGTH = "131072";
+      OLLAMA_KEEP_ALIVE = "0";
 
-      # 2. REQUIRED FOR 128K: Compresses context memory so it fits in 16GB VRAM
+      # Set the global default context length to 128k
+      OLLAMA_CONTEXT_LENGTH = "131072";
+      # REQUIRED FOR 128K: Compresses context memory so it fits in 16GB VRAM
       OLLAMA_FLASH_ATTENTION = "1";
       OLLAMA_KV_CACHE_TYPE = "q4_0"; # 'q4_0' saves ~75% VRAM; use 'q8_0' for slightly better logic at ~50% savings
     };
